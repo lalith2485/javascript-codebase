5090cace87c6b2a749fe47dfd885948d88bad3d9
Lalith Prakash <155645454+lalith2485@users.noreply.github.com>
2024-12-25 11:30:54 +0530

    Update firstprogram.js

--- basic/firstprogram.js
+++ basic/firstprogram.js
@@ -1,2 +1,9 @@
+/*
+1. Create an HTML file in the current directory.
+2. Add this JavaScript file to the HTML file using the <script> tag.
+3. Open the HTML file in a browser.
+4. Inspect the browser using developer tools. [Right-click on the page and select "Inspect" (or press Ctrl+Shift+I / Cmd+Option+I)]
+5. Navigate to the "Console" tab to observe the output.
+*/
 
 console.log("jai sree ram");
